248a78b7fdb2ff533084ee6c410c71b94003ea77
Alejandro Vigna-Gomez <alejandro.vigna@metrics.digital>
2024-02-27 12:41:25 +0100

    Mostly annotating Evgeni's script

--- scripts/triple_che_eccentricities.py
+++ scripts/triple_che_eccentricities.py
@@ -4,7 +4,7 @@
 """
 Created on Tue Mar 29 16:58:54 2022
 
-@author: evgeni
+@author: evgeni grishin
 """
 import pandas as pd
 import numpy as np
@@ -18,67 +18,89 @@ import matplotlib.colors as colors
 rc('text', usetex=False)
 from astropy import constants as const
 # %%
+# CONSTANTS
+c          = const.c.cgs.value                # speed of light (c) in cgs
+msun       = const.M_sun.cgs.value            # solar mass (Msun) in cgs
+G          = const.G.cgs.value                # gravitational constant (G) in cgs
+au         = const.au.cgs.value               # astronomical unit (AU) in cgs
+rsun       = const.R_sun.cgs.value            # solar radius (Rsun) in cgs
+rsun_to_au = const.R_sun.value/const.au.value # solar radius in au
+day_to_s   = 86400                            # day in s
+
+
 # %%
-#define constants
-# c=2.99792458e10
-# msun=2e33
-# G=6.674e-8
-# au=1.5e13
-# rsun = 6.9e10
-
-c=const.c.cgs.value
-msun=const.M_sun.cgs.value
-G=const.G.cgs.value
-au=const.au.cgs.value
-rsun=const.R_sun.cgs.value
-
-# define some useful quantities
-
-#this is self explanatory - I hope
-def period_ratio(m1,m2,m3,a1,a2):
+# FUNCTIONS
+def period_ratio(m1, m2, m3, a1, a2):
+    # m1, m2 and m3 are the masses of the primary, secondary and tertiary, respectively
+    # a1 and a2 are the semi-major axes of the inner and outer orbit, respectively
+    # given that we calculate a ratio, the values of the variables can be in any units, as long as they are consistent with one another
     m_bin = m1 + m2
     m_tot = m_bin + m3
     return (a1/a2)**1.5 * (m_tot/m_bin)**0.5
 
 # how bad the double averaging is (e.g. Grishin, Perets and Fragione 2018; Luo et al., 2016)
-def eps_sa(m1,m2,m3,a1,a2,e2):
+def epsilon_sa(m1, m2, m3, a1, a2, e2):
+    # e2 is the eccentricity of the outer orbit
+    # [e2]=adim
     m_bin = m1+m2
     m_tot = m_bin + m3
     return (a1/a2/(1-e2**2))**1.5*m3/(m_tot*m_bin)**0.5
 
 # angular momentum ratio (e.g. Haim and Katz 2018)
-def eta(m1,m2,m3,a1,a2,e2):
+def eta(m1, m2, m3, a1, a2, e2):
     m_bin = m1+m2
     m_tot = m_bin + m3
     mu_in = m1*m2/m_bin
     mu_out = m3*m_bin/m_tot
     return mu_in/mu_out*(m_bin*a1/m_tot/a2/(1-e2**2))**0.5
 
-#importance of GR corrections (e.g. Liu, Munoz and Lai 2015)
-def eps_gr(m1,m2,m3,a1,a2,e2):
+# importance of GR corrections (e.g. Liu, Munoz and Lai 2015)
+def epsilon_gr(m1, m2, m3, a1, a2, e2):
+    # [m1]=[m2]=[m3]=Msun
+    # [a1]=[a2]=au
+    # [e2]=adim
     m_bin = m1+m2
-    rg_over_a1= G*m_bin*msun/c**2/(a1*au)
- #   print (rg_over_a1)
-    return 3 * m_bin*(1-e2**2)**1.5/m3*(a2/a1)**3 * rg_over_a1
+    rg_over_a1= G*m_bin*msun/c**2/(a1*au) 
+    return 3 * m_bin*((1-e2**2)**1.5)/m3*(a2/a1)**3 * rg_over_a1
 
-#importance of tidal corrections (e.g. Liu, Munoz and Lai 2015)
-def eps_tide(m1,m2,m3,a1,a2,e2, r1, r2, k1, k2):
+# importance of tidal corrections (e.g. Liu, Muñoz and Lai 2015)
+def epsilon_tide(m1, m2, m3, a1, a2, e2, r1, r2, k1, k2):
+    # [m1]=[m2]=[m3]=Msun
+    # [a1]=[a2]=au
+    # r1 and r2 are the radius of the primary and the secondary, respectively
+    # [r1]=[r2]=au
+    # k1 and k2 are the apsidal motion constants of the primary and the secondary, respectively
+    # [k1]=[k2]=adim
     m_bin = m1+m2
     b2 = a2 * (1-e2**2)**0.5
     return 15 * m_bin * b2**3 * (m1**2 * k1 * r1**5 + m2**2 * k2 * r2**5 ) / a1**8 / m1 / m2/ m3
 
-#octupole term (e.g. Naoz 2016 and many others)
-def eps_oct(m1,m2,a1,a2,e2):
+# octupole term (e.g. Naoz 2016 and many others)
+def epsilon_oct(m1, m2, a1, a2, e2):
     return (m1-m2)/(m1+m2)* (a1/a2) * e2/(1-e2**2)
 
+# ALEJANDRO: include a function to check for stability?
+
 # %%
-# calculate the maximal eccentricity according to Mangipudi et al., 2022 and the appendix with tides
-def get_ecc(m1, m2, m3, a1, a2, e2, r1, r2, k1, k2, cos_inc):
-    cosi_0 = cos_inc
-    ETA = eta(m1,m2,m3,a1,a2,e2)
-    eps_GR1 = eps_gr(m1, m2, m3, a1, a2, e2)
-    eps_SA1 = eps_sa(m1, m2, m3, a1, a2, e2)
-    eps_Tide1 = eps_tide(m1, m2, m3, a1, a2, e2, r1, r2, k1, k2)
+# calculate the maximal eccentricity according to Mangipudi et al. 2022 and the appendix with tides
+def get_maximal_eccentricity(m1, m2, m3, a1, a2, e2, r1, r2, k1, k2, cos_inc, debug):
+    # [m1]=[m2]=[m3]=Msun
+    # [a1]=[a2]=au
+    # [e2]=adim
+    # [r1]=[r2]=au
+    # [k1]=[k2]=adim
+    # [cos_inc]=adim
+    cosi_0    = cos_inc
+    ETA       = eta(m1, m2, m3, a1, a2, e2)
+    eps_GR1   = epsilon_gr(m1, m2, m3, a1, a2, e2)
+    eps_SA1   = epsilon_sa(m1, m2, m3, a1, a2, e2)
+    eps_Tide1 = epsilon_tide(m1, m2, m3, a1, a2, e2, r1, r2, k1, k2)
+    
+    if debug:
+        print("ETA=",ETA)
+        print("eps_GR1=",eps_GR1)
+        print("eps_SA1=",eps_SA1)  
+        print("eps_Tide1=",eps_Tide1)          
     
     def F(e_m=0.5,cosi_0=cosi_0, eta=ETA):
         return (1 - e_m**2)*(-3 + eta*cosi_0 + e_m**2*eta**2/4) + 5*(cosi_0 + eta*e_m**2/2)**2 - eps_SA1*(3/8)*( 
@@ -98,29 +120,40 @@ def get_ecc(m1, m2, m3, a1, a2, e2, r1, r2, k1, k2, cos_inc):
         return 1
     return a+da
 
-print (get_ecc(50, 50, 200, 0.2, 1, 0, 7/214, 7/214, 0.001, 0,0 ))
+test_e_max = get_maximal_eccentricity(45, 45, 200, 0.2, 1, 0, 10*rsun_to_au, 10*rsun_to_au, 0.02, 0.02, 0, True)
+print("e_max=",test_e_max)
 
 
 # %%
-#plot e_max for a particular choice of inclination cosinc - set e_maxplot = True
-#plot fractions of collisoins - uniform in cos inc with Ni inclinations
+# ALEJANDRO: this function I still need to check/clean
+# plot e_max for a particular choice of inclination cosinc - set e_maxplot = True
+# plot fractions of collisoins - uniform in cos inc with Ni inclinations
 # set fraction_plot = True
-#the outer grid of masses and separations is of size NN x NN
+# the outer grid of masses and separations is of size NN x NN
 
 #the system is colliding if the pericentre r_p < 1.3 * (r1+r2)
 # AVG: double check this previous condition
-def plot_grids(NN, Ni,period,m1,m2,r1,r2,k1,k2,cosinc,col):
+# the system is colliding if the pericentre r_p < 1.32*R_{RL}
+# Which corresponds to r1 >= 0.5002r_p
+
+def plot_grids(NN, Ni, period, m1, m2, r1, r2, k1, k2, cosinc, col, debug):
     # AVG: explain all things here
     # AVG: probably better a script to first calculate the grid and another to then plot
     e_maxplot = True
     fraction_plot = True
 
-#     m1=70; m2=70; 
-    # Change 86400 to a constant
-    a1 = (G*(m1+m2)*msun * (period * 86400)**2/4/np.pi/np.pi)**0.3333/au
-    r1 = r1 * rsun / au; r2 = r2 * rsun / au
-    m3 = np.logspace(0,4,NN)
-    a2 = np.logspace(np.log10(a1) + np.log10(2), np.log10(a1)+ np.log10(300), NN)
+    a1 = (G*(m1+m2)*msun * (period * day_to_s)**2/4/np.pi/np.pi)**0.3333/au # ALEJANDRO: [a1]=au?
+    r1 = r1 * rsun / au; r2 = r2 * rsun / au # [r1]=[r2]=au
+    m3 = np.logspace(0,2,NN)
+#     a2 = np.logspace(np.log10(a1) + np.log10(2), np.log10(a1)+ np.log10(300), NN)    
+    a2 = np.logspace(np.log10(0.1), np.log10(10), NN)    
+    # ALEJANDRO: what is with the np.log10(2)?
+
+    if debug:
+        print(r1,r2)
+        print(a1,np.min(a2),np.max(a2))
+        print(np.min(m3),np.max(m3))
+
     eccs = np.zeros(shape=[NN,NN])
     rps = np.zeros(shape=[NN,NN])
     fraction = np.zeros(shape=[NN,NN])
@@ -129,44 +162,48 @@ def plot_grids(NN, Ni,period,m1,m2,r1,r2,k1,k2,cosinc,col):
     for i in range(0,NN):
         print (i)
         for j in range(0,NN):
-            eccs[i,j] = get_ecc(m1,m2,m3[i],a1,a2[j],0,r1 ,r2,k1,k2, cosinc) 
+            eccs[i,j] = get_maximal_eccentricity(m1, m2, m3[i], a1, a2[j], 0, r1 ,r2, k1, k2, cosinc, False) 
             rps[i,j] = a1 * (1 - eccs[i,j]) * au / rsun
             counter=0
             for k in range(0,Ni):
-               ec = get_ecc(m1,m2,m3[i],a1,a2[j],0,r1 ,r2,k1,k2, cos_incs[k])
-               rpp = a1 * (1 - ec) 
+                ec = get_maximal_eccentricity(m1, m2, m3[i], a1, a2[j], 0, r1 ,r2, k1, k2, cos_incs[k], False)
+                rpp = a1 * (1 - ec) 
+#                 print("rpp=",rpp)
 
-               if rpp/1.3/(r1+r2)<=1:
-                   counter=counter+1
+                # Which corresponds to r1 >= 0.5002r_p
+#                 if rpp/1.3/(r1+r2)<=1:
+                if r1 >= 0.5002*rpp or r2 >= 0.5002*rpp:
+                    counter=counter+1
 
             fraction[i,j] = counter/Ni       
                 
-
     plt.rc('text', usetex=False)
     matplotlib.rcParams.update({'font.size': 20})
     if e_maxplot:
-        plt.contourf(np.log10(a2), np.log10(m3), eccs, levels=np.linspace(0,1,21))
-        plt.text(1., 4.03, r'$e_{\rm max}$')
-        plt.xlabel(r'$\log a_2 / \rm au$')
-        plt.ylabel(r'$\log m_2 / \rm M_\odot$')
+#         plt.contourf(np.log10(m3), np.log10(a2), eccs, levels=np.linspace(0,1,21))
+        plt.contourf(np.log10(m3), np.log10(a2), eccs)
+        plt.text(2.03, 1., r'$e_{\rm max}$')
+        plt.xlabel(r'$\log m_3 / \rm M_\odot$')
+        plt.ylabel(r'$\log a_2 / \rm au$')
         plt.colorbar()
-        plt.subplots_adjust(left=0.18, bottom=0.15, right=0.97, top=0.93)
+#         plt.subplots_adjust(left=0.18, bottom=0.15, right=0.97, top=0.93)
 
     #   plt.contour((a2), np.log10(m3), rps/(r1+r2) * rsun/au, [1.3], colors=col) #[2,2.5,3,3.5,4,4.5])
     if fraction_plot:
         plt.figure(2)
-        plt.contourf(np.log10(a2), np.log10(m3), fraction) #[2,2.5,3,3.5,4,4.5])
-        plt.text(1., 4.02, 'fraction')
-        plt.xlabel(r'$\log a_2 / \rm au$')
-        plt.ylabel(r'$\log m_2 / \rm M_\odot$')
+        plt.contourf(np.log10(m3), np.log10(a2), fraction) #[2,2.5,3,3.5,4,4.5])
+        plt.text(2.02, 1., 'fraction')
+        plt.xlabel(r'$\log m_3 / \rm M_\odot$')
+        plt.ylabel(r'$\log a_2 / \rm au$')
         plt.colorbar()
-        plt.subplots_adjust(left=0.18, bottom=0.15, right=0.97, top=0.93)
+#         plt.subplots_adjust(left=0.18, bottom=0.15, right=0.97, top=0.93)
 
 
     #  plt.colorbar(format="%.2f")
  #   plt.text(4,150 + 60*cosinc, str(cosinc), color=col)
     return eccs, rps, fraction
 
+# ALEJANDRO: can I erase the following lines?
 # #%%
 # NN=10
 # a1=0.16
@@ -201,7 +238,16 @@ def plot_grids(NN, Ni,period,m1,m2,r1,r2,k1,k2,cosinc,col):
 # plt.subplots_adjust(left=0.15, bottom=0.16, right=0.97, top=0.92)
 
 # %%
-NN=40; Ni=30; r1=10; r2=10; m1=45; m2=45; k1=0.02; k2=0.02;
-res = plot_grids(NN,Ni,2,m1,m2,r1,r2,k1,k2,0.5, 'red')
+NN=40; Ni=30; period_days=3; m1=45; m2=45; r1=10; r2=10; k1=0.02; k2=0.02;
+res = plot_grids(NN, Ni, period_days, m1, m2, r1, r2, k1, k2, 0.0, 'red', False)
+
+# %%
+print(get_maximal_eccentricity(45, 45, 100, 0.18245, 0.4, 0, 10*rsun_to_au, 10*rsun_to_au, 0.02, 0.02, 0, 1))
+
+# %%
+epsilon_tide(45, 45, 100, 0.18245, 0.4, 0.0, 10*rsun_to_au, 10*rsun_to_au, 0.02, 0.02)
+
+# %%
+epsilon_gr(45, 45, 100, 0.18245, 0.4, 0.0)
 
 # %%
